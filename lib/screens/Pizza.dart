4a3d258ce2652bce80920d29dc8dbbb25742f58a
Craig Mills <craig.mills@288group.com>
2021-07-12 17:23:54 +0100

    Adding custom fonts

--- lib/screens/Pizza.dart
+++ lib/screens/Pizza.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Pizza extends StatelessWidget
@@ -11,7 +13,15 @@ class Pizza extends StatelessWidget
       //height: 96.0,
       //margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 50.0, bottom: 50.0),
       margin: EdgeInsets.all(75.0),
-      child: Text("Pizza", textDirection: TextDirection.ltr,)
+      child: Text("Pizza", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 60.0,
+        decoration: TextDecoration.none,
+        fontFamily: 'Zen Tokyo Zoo',
+        fontWeight: FontWeight.w300,
+        ),
+      )
     ));
   }
 }
