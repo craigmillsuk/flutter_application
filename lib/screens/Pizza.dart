13bd23d2a1a04f2b057e8253e8015f5486c59de0
Craig Mills <craig.mills@288group.com>
2021-07-15 16:12:59 +0100

    Structure changes

--- lib/screens/Pizza.dart
+++ lib/screens/Pizza.dart
@@ -1,105 +1,10 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:hello_world/widgets/PizzaColumn.dart';
 
 class Pizza extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:Container(
-      alignment: Alignment.center,
-      padding: EdgeInsets.only(top:30.0, left:15.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blueAccent),
-        color: Colors.deepOrangeAccent,
-      ),
-      child: Column(children: <Widget>[
-            Row(children: <Widget>[
-              Expanded(child: Text("Margerhita", 
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                fontSize: 30.0,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            ),
-              Expanded(child: Text("Tomato, Mozarella, Basil", 
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                fontSize: 20.0,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w300,
-                ),
-              ),
-              )
-          ]),
-          Row(children: <Widget>[
-              Expanded(child: Text("Marinara", 
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                fontSize: 30.0,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            ),
-              Expanded(child: Text("Tomato, Garlic", 
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                fontSize: 20.0,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w300,
-                ),
-              ),
-              )
-          ]),
-          PizzaImageWidget(),
-          OrderButton(),
-      ]),
-    ));
-  }
-}
-
-class PizzaImageWidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    AssetImage pizzaAsset = new AssetImage('images/pizza.png');
-    Image image = Image(image: pizzaAsset, width: 200.0, height: 200.0);
-    return Container(child: image,
-      margin: EdgeInsets.only(top: 100.0),
-      alignment: Alignment.center,
-    );
-  }
-}
-
-class OrderButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-   var button = Container(
-     margin: EdgeInsets.only(top: 50.0),
-     child: RaisedButton(
-       child: Text("Order your pizza"),
-       color: Colors.lightGreen,
-       elevation: 5.0,
-       onPressed: () {
-         order(context);
-       },
-     )
-   );
-   return button;
-  }
-
-  void order(BuildContext context)
-  {
-    var alert = AlertDialog(
-      title: Text("Order Completed"),
-      content: Text("Thank you for your order"),
-    );
-    showDialog(
-      context: context, 
-      builder: (BuildContext builder) => alert
-    );
+    return PizzaColumnWidget();
   }
 }
