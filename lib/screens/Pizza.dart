681afa5588ac2e62a47d8c976520ac75298f6ab1
Craig Mills <craig.mills@288group.com>
2021-07-15 15:50:14 +0100

    Pizza order button

--- lib/screens/Pizza.dart
+++ lib/screens/Pizza.dart
@@ -56,6 +56,7 @@ class Pizza extends StatelessWidget
               )
           ]),
           PizzaImageWidget(),
+          OrderButton(),
       ]),
     ));
   }
@@ -69,6 +70,36 @@ class PizzaImageWidget extends StatelessWidget{
     return Container(child: image,
       margin: EdgeInsets.only(top: 100.0),
       alignment: Alignment.center,
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   var button = Container(
+     margin: EdgeInsets.only(top: 50.0),
+     child: RaisedButton(
+       child: Text("Order your pizza"),
+       color: Colors.lightGreen,
+       elevation: 5.0,
+       onPressed: () {
+         order(context);
+       },
+     )
+   );
+   return button;
+  }
+
+  void order(BuildContext context)
+  {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thank you for your order"),
+    );
+    showDialog(
+      context: context, 
+      builder: (BuildContext builder) => alert
     );
   }
 }
