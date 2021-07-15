abc1fc9d873a6f7f479659e689321fdf15ac133e
Craig Mills <craig.mills@288group.com>
2021-07-15 14:04:10 +0100

    Pizza image added - Cherry pick

--- lib/screens/Pizza.dart
+++ lib/screens/Pizza.dart
@@ -54,8 +54,21 @@ class Pizza extends StatelessWidget
                 ),
               ),
               )
-          ])
+          ]),
+          PizzaImageWidget(),
       ]),
     ));
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = new AssetImage('images/pizza.png');
+    Image image = Image(image: pizzaAsset, width: 200.0, height: 200.0);
+    return Container(child: image,
+      margin: EdgeInsets.only(top: 100.0),
+      alignment: Alignment.center,
+    );
   }
 }
